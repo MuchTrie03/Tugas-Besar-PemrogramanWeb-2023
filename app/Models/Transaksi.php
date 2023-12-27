b204066f45046090e8d04ce4030ef46717e5eaef
AndhikaF29 <andhikafajarprayoga@gmail.com>
2023-12-27 08:39:03 +0700

    + model

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    protected $table = 'transaksi'; // Ganti dengan nama tabel yang benar
+
+    // Definisi lain dari model Transaksi
+}
+
